196472c07bd25554ca0690c347678950e5a23c49
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-10 23:14:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-     res.send(createtemplate(article-One));
+     res.send(createtemplate(articledata));
 });
 
 
