6eb7c36e0b0370dc4ef59145f030a64573bdbe75
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-10 23:06:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,25 +10,25 @@ var articles = {
 title: 'Article One',
 heading: 'Article One',
 date: 'Aug 18 2017',
-content: <p>This is the paragraph tag.</p>
+content:` <p>This is the paragraph tag.</p>
 <p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>
+<p>This is One more paragraph tag.</p>`
 },
 'articletwo' : {
 title: 'Article Two',
 heading: 'Article Two',
 date: 'Aug 20 2017',
-content: <p>This is the paragraph tag.</p>
+content:` <p>This is the paragraph tag.</p>
 <p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>
+<p>This is One more paragraph tag.</p>`
 },
 'articlethree' : {
 title: 'Article Three',
 heading: 'Article Three',
 date: 'Aug 22 2017',
-content: <p>This is the paragraph tag.</p>
+content: `<p>This is the paragraph tag.</p>
 <p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>
+<p>This is One more paragraph tag.</p>`
 }
 };
 
@@ -86,5 +86,5 @@ res.send(createtemplate(articles[articleName]));
 
 var port = 80;
 app.listen(port, function () {
-console.log(IMAD course app listening on port ${port}!);
+console.log(`IMAD course app listening on port ${port}!`);
 });
