8433710eb644fa10d1934a315e8b910aff9111d4
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-10 23:12:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,31 +5,15 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = { 
-'articleone' : {
+var articleone = { 
+
 title: 'Article One',
 heading: 'Article One',
 date: 'Aug 18 2017',
 content:` <p>This is the paragraph tag.</p>
 <p>This is Another paragraph tag.</p>
 <p>This is One more paragraph tag.</p>`
-},
-'articletwo' : {
-title: 'Article Two',
-heading: 'Article Two',
-date: 'Aug 20 2017',
-content:` <p>This is the paragraph tag.</p>
-<p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>`
-},
-'articlethree' : {
-title: 'Article Three',
-heading: 'Article Three',
-date: 'Aug 22 2017',
-content: `<p>This is the paragraph tag.</p>
-<p>This is Another paragraph tag.</p>
-<p>This is One more paragraph tag.</p>`
-}
+
 };
 
 function createtemplate (data){
