d5e10ea826215fcdb9b4f592caef1b28a4dc7206
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-10 21:01:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,15 +11,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Article one requested and will besaved here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
-    res.send('Article two requested and will besaved here');
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-   res.send('Article three requested and will besaved here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
