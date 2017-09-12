875fb1d93b862d6ba41c163776688b07346bc989
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-12 22:11:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/favicon.ico', function (req, res) {
-res.sendFile(path.join(__dirnam, 'ui','favicon.ico'));
+res.sendFile(path.join(__dirname, 'ui','favicon.ico'));
 });
 
 
