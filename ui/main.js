520dd26558dcf2a2a7bffa8071b997787f082873
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-13 13:34:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,28 @@
-function exec(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() 
-    {
-          if (request.readyState === XMLHttpRequest.DONE) 
-          {
-              //Take Some Action
-              if (request.status === 200) 
-                { 
-                    var counter = request.responseText; 
-                    var span = document.getElementById('count');
-                    span.innerHTML = counter.toString();
-                } 
-          } 
-    }; 
-    request.open("GET", "http://pranalisawarkar95.imad.hasura-app.io/counter", true);
-    request.send(); 
-    console.log('EXECUTED');
-}
+//counter code
+
+var button = document.getElementById("counter");
+
+button.onClick = function() {
+   
+ //Create A Request oject 
+ 
+ var request = new XMLHttpRequest();
+ 
+ //Capture The Response And Store In HTML
+ 
+ request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+      
+      //Take Some Action
+          if (request.status === 200) {
+              var counter= request.responseText; 
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+          }
+      }
+      // not done yet
+ };
+ //Make A Request
+  request.open('GET', 'http://kogam22.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
