b81f49bcdb954422a188f40607b1061b4b0cea31
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-13 13:24:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,38 @@
-console.log('Loaded!');
-
-
-//change the text of the main-text div
-
-var element = document.getElementById('main-text');
-    element.innerHTML = 'New Value';
-
-//move the image
-
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-     marginLeft= marginLeft+1;
-      img.style.marginLeft =marginLeft +'px';
-}
-img.onclick = function() {
-      var interval = setInterval(moveRight, 50);
-
+//counter code
+console.log("JS Loaded Successfully!");
+var button = document.getElementById("counter");
+console.log("\"counter\" Is Selected Now");
+button.onClick = function() {
+  
+  console.log("Button Was Clicked");
+  //Create A Request To Counter Endpoint
+  var request = new XMLHttpRequest();
+  console.log("Request Was Created");
+  //Capture The Response And Store In HTML
+  request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) 
+      {   
+          console.log("Checking If Request Was Done");
+          //Take Some Action
+          if (request.status === 200) 
+            { 
+                console.log("Check If status Is Completed");
+                var counter= request.responseText; 
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+                console.log("counter Was Sent To Span");
+            } 
+          else 
+          { 
+              alert('There was a problem with the request.');
+              console.log("An Error Occurred With The Request.");
+              
+          }
       
+      } 
+      
+  };
+  //Make A Request
+  request.open('GET', 'http://kogam22.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
