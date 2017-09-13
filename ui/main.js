217e5b58a4286ff3343e987275a8fea6fc7906da
pranalisawarkar95 <31503099+pranalisawarkar95@users.noreply.github.com>
2017-09-13 13:28:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,38 +1,19 @@
-//counter code
-console.log("JS Loaded Successfully!");
-var button = document.getElementById("counter");
-console.log("\"counter\" Is Selected Now");
-button.onClick = function() {
-  
-  console.log("Button Was Clicked");
-  //Create A Request To Counter Endpoint
-  var request = new XMLHttpRequest();
-  console.log("Request Was Created");
-  //Capture The Response And Store In HTML
-  request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) 
-      {   
-          console.log("Checking If Request Was Done");
-          //Take Some Action
-          if (request.status === 200) 
-            { 
-                console.log("Check If status Is Completed");
-                var counter= request.responseText; 
-                var span = document.getElementById("count");
-                span.innerHTML = counter.toString();
-                console.log("counter Was Sent To Span");
-            } 
-          else 
-          { 
-              alert('There was a problem with the request.');
-              console.log("An Error Occurred With The Request.");
-              
-          }
-      
-      } 
-      
-  };
-  //Make A Request
-  request.open('GET', 'http://pranalisawarkar95.imad.hasura-app.io/counter', true);
-  request.send(null);
-};
+function exec(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://pranalisawarkar95.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
+}
